3f7d27200dc83b06a4a6e827bf8d39f41fb83bb4
My Name <azs030369@gmail.com>
2023-01-13 18:55:00 +0900

    call data from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home() {
+  const [drinks, setDrinks] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+        )
+      ).json();
+      console.log(data);
+    })();
+  }, []);
   return (
     <div>
-        <Seo title="home" />
+      <Seo title="home" />
       <h1 className="active">カクテルの種類</h1>
     </div>
   );
